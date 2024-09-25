273024e4b4c58af9db21c6e21f3950f8f31714d2
sukreshmanda <mandasukresh@gmail.com>
2024-09-26 00:09:30 +0530

    Add CodeQL analyser for FromBody attribute

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -4,21 +4,5 @@ namespace CatsAPI.Controllers;
 
 public class BaseController: ControllerBase
 {
-    [HttpPost]
-    [Route("/login")]
-    public void Login([FromBody] MessageRequest loginCredentials)
-    {
-        return;
-    }
-
-    [HttpPost("/logout")]
-    public void Logout([FromBody] MessageRequest logout)
-    {
-        return;
-    }
-
-    public class MessageRequest{
-        public string? Name { get; set; }
-        public long? Id { get; set; }
-    }
+    
 }
--- Controllers/SecondaryController.cs
+++ Controllers/SecondaryController.cs
@@ -6,5 +6,21 @@ namespace CatsAPI.Controllers;
 [Route("[controller]")]
 public class SecondaryController : BaseController
 {
+    [HttpPost]
+    [Route("/login")]
+    public void Login([FromBody] MessageRequest loginCredentials)
+    {
+        return;
+    }
 
+    [HttpPost("/logout")]
+    public void Logout(MessageRequest logout)
+    {
+        return;
+    }
+
+    public class MessageRequest{
+        public string? Name { get; set; }
+        public long? Id { get; set; }
+    }
 }
